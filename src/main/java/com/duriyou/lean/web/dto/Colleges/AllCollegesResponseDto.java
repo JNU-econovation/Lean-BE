386ccb33723faa83cdc0a9150491d7c299397916
LeeJS9856 <wnstj1057@naver.com>
2025-01-07 22:01:15 +0900

    Refactor : AllCollegesResponse 관련 리팩토링

--- src/main/java/com/duriyou/lean/web/dto/Colleges/AllCollegesResponseDto.java
+++ src/main/java/com/duriyou/lean/web/dto/Colleges/AllCollegesResponseDto.java
@@ -3,16 +3,13 @@ package com.duriyou.lean.web.dto.Colleges;
 import com.duriyou.lean.domain.colleges.Colleges;
 import lombok.Getter;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Getter
 public class AllCollegesResponseDto {
-    private final List<CollegesResponseDto> colleges;
+    private final Long id;
+    private final String name;
 
-    public AllCollegesResponseDto(List<Colleges> colleges) {
-        this.colleges = colleges.stream()
-                .map(CollegesResponseDto::new)
-                .collect(Collectors.toList());
+    public AllCollegesResponseDto(Colleges college) {
+        this.id = college.getId();
+        this.name = college.getName();
     }
 }
