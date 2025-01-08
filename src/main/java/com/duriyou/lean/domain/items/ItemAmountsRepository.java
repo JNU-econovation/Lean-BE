c880f26545d783fffe1979a031a8167a36d46f24
xxbeann <yjb4945@gmail.com>
2025-01-09 03:16:47 +0900

    feat: ItemAmountsRepository 작성

---
+++ src/main/java/com/duriyou/lean/domain/items/ItemAmountsRepository.java
@@ -0,0 +1,6 @@
+package com.duriyou.lean.domain.items;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemAmountsRepository extends JpaRepository<ItemAmounts, Long> {
+}
