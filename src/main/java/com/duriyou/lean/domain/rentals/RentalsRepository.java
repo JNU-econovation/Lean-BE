0cfe98a5bbab94f4b985b786235a90b79b8564fa
xxbeann <yjb4945@gmail.com>
2025-01-09 02:37:44 +0900

    feat: RentalsRepository 생성

---
+++ src/main/java/com/duriyou/lean/domain/rentals/RentalsRepository.java
@@ -0,0 +1,6 @@
+package com.duriyou.lean.domain.rentals;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RentalsRepository extends JpaRepository<Rentals, Long> {
+}
