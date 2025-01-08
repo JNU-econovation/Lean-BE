5999f0a39e35e74285ad4bf39c56aa94df509557
xxbeann <yjb4945@gmail.com>
2025-01-09 02:30:27 +0900

    feat: Items Entity 생성

---
+++ src/main/java/com/duriyou/lean/domain/items/Items.java
@@ -0,0 +1,24 @@
+package com.duriyou.lean.domain.items;
+
+import com.duriyou.lean.domain.student.council.StudentCouncil;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Items {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_council_id")
+    private StudentCouncil studentCouncil;
+
+
+    @Column(nullable = false)
+    private String name;
+}
