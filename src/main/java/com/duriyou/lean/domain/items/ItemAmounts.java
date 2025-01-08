3c6c04e03de51ad35a2075746e2592e1e10bfa16
xxbeann <yjb4945@gmail.com>
2025-01-09 03:14:52 +0900

    feat: ItemAmounts Entity 작성

---
+++ src/main/java/com/duriyou/lean/domain/items/ItemAmounts.java
@@ -0,0 +1,29 @@
+package com.duriyou.lean.domain.items;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class ItemAmounts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "item_id")
+    private Items items;
+
+    @Column(nullable = false)
+    private Integer amount;
+
+    @Builder
+    public ItemAmounts(Items items, Integer amount){
+        this.items = items;
+        this.amount = amount;
+    }
+}
