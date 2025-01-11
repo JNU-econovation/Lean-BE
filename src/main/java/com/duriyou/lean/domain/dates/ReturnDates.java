21f902c8ffdec39917d6eca658878b386f98aa4a
xxbeann <yjb4945@gmail.com>
2025-01-12 00:50:36 +0900

    feat: ReturnDates & ReturnDatesRepository작성

---
+++ src/main/java/com/duriyou/lean/domain/dates/ReturnDates.java
@@ -0,0 +1,40 @@
+package com.duriyou.lean.domain.dates;
+
+import com.duriyou.lean.domain.rentals.Rentals;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class ReturnDates {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "rental_id")
+    private Rentals rentals;
+
+    // Date - 사용자가 반납한 날짜
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime date;
+
+    @PrePersist
+    public void prePersist(){
+        if (date == null){
+            date = LocalDateTime.now();
+        }
+    }
+
+    @Builder
+    public ReturnDates(Rentals rentals, LocalDateTime date){
+        this.rentals = rentals;
+        this.date = date;
+    }
+}
