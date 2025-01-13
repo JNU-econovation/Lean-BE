5d5e68667416b0d8298772de0ab98b078ac9d8bf
xxbeann <yjb4945@gmail.com>
2025-01-14 06:05:23 +0900

    feat: 유저의 모든 대여내역 조회하는 api 개발

--- src/main/java/com/duriyou/lean/service/rentals/RentalsService.java
+++ src/main/java/com/duriyou/lean/service/rentals/RentalsService.java
@@ -1,20 +1,37 @@
-//package com.duriyou.lean.service.rentals;
-//
-//import com.duriyou.lean.domain.users.Users;
-//import com.duriyou.lean.domain.users.UsersRepository;
-//import com.duriyou.lean.web.dto.rentals.RentalsResponseDto;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//@RequiredArgsConstructor
-//@Service
-//public class RentalsService {
-//
-//    private final UsersRepository usersRepository;
-//
-//    public RentalsResponseDto findById(Long id){
-//        Users entity = usersRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
-//        return new RentalsResponseDto(entity);
-//    }
-//
-//}
+package com.duriyou.lean.service.rentals;
+
+import com.duriyou.lean.domain.users.UsersRepository;
+import com.duriyou.lean.web.dto.rentals.UserAllRentalsDto;
+import com.duriyou.lean.web.dto.rentals.UserAllRentalsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class RentalsService {
+
+    private final UsersRepository usersRepository;
+
+    public List<UserAllRentalsResponseDto> findUserAllRentalsById(Long user_id){
+        // JPQL로 데이터 조회
+        List<UserAllRentalsDto> userAllRentalsDtos = usersRepository.findUserAllRentals(user_id);
+
+        // ResponseDto로 변환
+        return userAllRentalsDtos.stream()
+                .map(userAllRentalsDto -> new UserAllRentalsResponseDto(
+                        userAllRentalsDto.getStudent_council_name(),
+                        userAllRentalsDto.getRental_id(),
+                        userAllRentalsDto.getRental_status(),
+                        userAllRentalsDto.getItem_id(),
+                        userAllRentalsDto.getItem_name(),
+                        userAllRentalsDto.getRental_date_expiration()
+                ))
+                .collect(Collectors.toList());
+
+
+    }
+
+}
--- src/main/java/com/duriyou/lean/web/RentalsApiController.java
+++ src/main/java/com/duriyou/lean/web/RentalsApiController.java
@@ -1,21 +1,26 @@
-//package com.duriyou.lean.web;
-//
-//import com.duriyou.lean.service.rentals.RentalsService;
-//import com.duriyou.lean.web.dto.rentals.RentalsResponseDto;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RequiredArgsConstructor
-//@RestController
-//public class RentalsApiController {
-//
-//    private final RentalsService rentalsService;
-//
-//    @GetMapping("api/v1/rentals/{user_id}")
-//    public RentalsResponseDto findById(@PathVariable Long id) {
-//        return RentalsService.findById(id);
-//    }
-//
-//}
+package com.duriyou.lean.web;
+
+import com.duriyou.lean.service.rentals.RentalsService;
+import com.duriyou.lean.web.dto.rentals.UserAllRentalsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class RentalsApiController {
+
+    private final RentalsService rentalsService;
+
+    // R - 01
+    @GetMapping("api/v1/rentals/{user_id}")
+    public ResponseEntity<List<UserAllRentalsResponseDto>> findUserAllRentalsById(@PathVariable Long user_id) {
+        List<UserAllRentalsResponseDto> response = rentalsService.findUserAllRentalsById(user_id);
+        return ResponseEntity.ok(response);
+    }
+
+}
