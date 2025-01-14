df6398a09fbf929c11b241035c77a230402bcab5
xxbeann <yjb4945@gmail.com>
2025-01-14 09:05:00 +0900

    refactor: JPQL 텍스트 블록 방식으로 재작성

--- src/main/java/com/duriyou/lean/domain/student/council/StudentCouncilRepository.java
+++ src/main/java/com/duriyou/lean/domain/student/council/StudentCouncilRepository.java
@@ -8,6 +8,22 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface StudentCouncilRepository extends JpaRepository<StudentCouncil, Long> {
+    // 텍스트 블록 방식 + ON 조건 명시
+    @Query("""
+       SELECT new com.duriyou.lean.web.dto.rentals.StudentCouncilAllRentalsDto(
+           u.name,
+           i.name,
+           r.status,
+           r.id
+       )
+       FROM StudentCouncil sc
+       RIGHT JOIN Items i ON sc.id = i.studentCouncil.id
+       RIGHT JOIN Rentals r ON i.id = r.items.id
+       LEFT JOIN Users u ON u.id = r.users.id
+       WHERE sc.id = :student_council_id
+       ORDER BY r.id ASC
+       """)
+    /*
     @Query("SELECT new com.duriyou.lean.web.dto.rentals.StudentCouncilAllRentalsDto( " +
             "u.name, " +
             "i.name, " +
@@ -20,5 +36,6 @@ public interface StudentCouncilRepository extends JpaRepository<StudentCouncil, 
             "LEFT JOIN Users u ON u.id = r.users.id " +
             "WHERE sc.id = :student_council_id " +
             "ORDER BY r.id ASC")
+     */
     List<StudentCouncilAllRentalsDto> findStudentCouncilAllRentalsById(@Param("student_council_id") Long student_council_id);
 }
