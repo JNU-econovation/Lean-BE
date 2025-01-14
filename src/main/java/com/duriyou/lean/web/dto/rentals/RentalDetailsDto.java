6cfce7f096c2f24942a899428aa7e48cb2e32839
xxbeann <yjb4945@gmail.com>
2025-01-14 09:06:29 +0900

    feat: RentalDetailsDto 작성
    JPQL활용해서 데이터 가져올때 직접적으로 매핑되는 DTO 입니다.

---
+++ src/main/java/com/duriyou/lean/web/dto/rentals/RentalDetailsDto.java
@@ -0,0 +1,19 @@
+package com.duriyou.lean.web.dto.rentals;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class RentalDetailsDto {
+    private String studentCouncilName;
+    private String studentCouncilAddress;
+    private String itemName;
+    private LocalDateTime rentalStartDate;
+    private LocalDateTime rentalExpireDate;
+    private String userName;
+    private String userCollegeName;
+    private String userPhoneNumber;
+}
