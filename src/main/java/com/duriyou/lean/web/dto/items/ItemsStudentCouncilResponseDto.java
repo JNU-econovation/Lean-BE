fecc3a51e26027a2cb7a24259608b4788bff80f2
xxbeann <yjb4945@gmail.com>
2025-01-19 22:31:28 +0900

    ItemsStudentCouncilResponseDto에 collegeName필드 추가

--- src/main/java/com/duriyou/lean/web/dto/items/ItemsStudentCouncilResponseDto.java
+++ src/main/java/com/duriyou/lean/web/dto/items/ItemsStudentCouncilResponseDto.java
@@ -10,11 +10,13 @@ public class ItemsStudentCouncilResponseDto {
 
     private String studentCouncilName;
     private String studentCouncilAddress;
+    private String collegeName;
     private List<ItemsNameResponseDto> items;
 
     public ItemsStudentCouncilResponseDto(StudentCouncil entity, List<ItemsNameResponseDto> items) {
         this.studentCouncilName = entity.getName();
         this.studentCouncilAddress = entity.getAddress();
+        this.collegeName = entity.getUser().getCollege().getName();
         this.items = items;
     }
 }
