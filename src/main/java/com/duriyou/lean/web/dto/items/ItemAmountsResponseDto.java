92d1c6534ffe742f7ddabe95810a3620e6effd7d
xxbeann <yjb4945@gmail.com>
2025-01-20 01:53:41 +0900

    feat: 대여중, 대여 예약, 총량 담을 수 있게 ItemAmountsResponseDto 수정

--- src/main/java/com/duriyou/lean/web/dto/items/ItemAmountsResponseDto.java
+++ src/main/java/com/duriyou/lean/web/dto/items/ItemAmountsResponseDto.java
@@ -7,8 +7,14 @@ import lombok.Getter;
 public class ItemAmountsResponseDto {
 
     private final Integer itemAmount;
+    private final Integer processAmount;
+    private final Integer rentAmount;
+    private final Integer stock;
 
-    public ItemAmountsResponseDto(ItemAmounts entity){
+    public ItemAmountsResponseDto(ItemAmounts entity, Integer processAmount, Integer rentAmount, Integer stock){
         this.itemAmount = entity.getAmount();
+        this.processAmount = processAmount;
+        this.rentAmount = rentAmount;
+        this.stock = stock;
     }
 }
